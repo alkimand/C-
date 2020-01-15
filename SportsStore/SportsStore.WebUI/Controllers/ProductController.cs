946306e4ca2a887c4e9a0289b360af4ee185e789
home <alkimand@mail.ru>
2020-01-15 23:51:30 +0300

    tmp

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,18 +5,46 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+//namespace SportsStore.WebUI.Models
+//{
+//    public class ProductsListViewModel
+//    {
+//        public IEnumerable<Product> Products { get; set; }
+//        public PagingInfo PagingInfo { get; set; }
+//    }
+//}
+
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
-        public ViewResult List()
+        public ViewResult List(string category, int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .Where(p => category == null ||
+                p.Category == category)
+                .OrderBy(p => p.ProductID).
+                Skip((page - 1) * PageSize).
+                Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                },
+                CurrentCategory = category
+            };
+            return View(model);
         }
     }
 }
